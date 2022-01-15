c04071dfed6334147028581ff98576944c28e47a
Arkadiusz Kruszynski <arkadiusz.h.kruszynski@gmail.com>
2022-01-15 10:44:44 +0100

    Add waiting for a mission

--- TelloBird.py
+++ TelloBird.py
@@ -1,26 +1,49 @@
 from tello import Tello
+import cv2
 
 
 class TelloBird(Tello):
     currentState = "landed"
+    statusOfMission = False
     listOfStates = ["landed", "in-air","moving","turning","too-weak"]
+    listOfMissions = ["BasicMisssionL", "SquareMissionL"]
     minimalTimeWaiting = 3.0
 
 
 
     def __init__(self):
         super().__init__()
+        self.detector = cv2.QRCodeDetector()
         self.wait(self.minimalTimeWaiting)
         self.land()
-        self.getData()
+        self.__getData__()
         if self.battery <= 10:
             print("Too low battery, recharge: ", self.battery)
             self.currentState = self.listOfStates[-1]
         
+    def WatingForMission(self):
+        if self.statusOfMission == True:
+            return 0
+        if self.last_frame is not None:
+                data, bbox, straigt_qrcode = self.detector.detectAndDecode(self.last_frame)
+        if bbox is not None:
+            print(data)
+            commands = self.__validadtionCommand__(data.split())
+            index = self.listOfMissions.index(commands[0])
+            self.StartMission(index,commands[1])
+            self.statusOfMission = True
+
+    def __endingMission__(self):
+        self.statusOfMission = False
+
+    def __validadtionCommand__(self, commands):
+        if len(commands) > 1:
+            return commands
+        commands.append("None")
+        return commands
 
 
-
-    def getData(self):
+    def __getData__(self):
         self.battery = self.get_battery()
         """TODO: Add more settings"""
 
@@ -37,10 +60,13 @@ class TelloBird(Tello):
             self.currentState = self.listOfStates[1]
             self.land()
             self.currentState = self.listOfStates[0]
+            self.__endingMission__()
             return 1
         else:
             print("I can't start a mission, I am flying")
+            self.__endingMission__()
             return 0
+
 
     def SquareMissionL(self, distance):
         if self.currentState == self.listOfStates[0]:
@@ -54,9 +80,15 @@ class TelloBird(Tello):
             self.land()
             self.currentState = self.listOfStates[1]
         else:
-            print("Something goes wrong mate :(")
+            print("Something went wrong mate :(")
+        self.__endingMission__()
 
 
+    def StartMission(self, chooseMission: int, parametr):
+        if chooseMission == 0:
+            self.BasicMisssionL()
+        elif chooseMission == 1:
+            self.SquareMissionL(parametr)
 
 
 
--- main.py
+++ main.py
@@ -7,49 +7,31 @@ import cv2
 import time
 import datetime
 
-
-
-
 drone = TelloBird()
+
 start = True
+bbox = None
 
 try:
-    while start:
+    while True:
         if start == True:
-            # drone.streamon()
-            drone.takeoff()
-            print(drone.get_battery())
+            drone.streamon()
+            # drone.takeoff()
+            # print(drone.get_battery())
             start = False
-            drone.wait(3)
-    # if drone.last_frame is not  None:
-    #     data, bbox, straigt_qrcode = detector.detectAndDecode(drone.last_frame)
-    #     if bbox is not None:
-    #         print(data)
 
 
+        drone.WatingForMission()
 
-        for i in range(4):
-            drone.cw(90)
-            drone.wait(3)
-            print(drone.get_speed())
-        # temp = drone.get_log()
-        # print(temp[-1].get_response())
-        # if str(temp[-1].get_response()) == "b'ok'":
-        #     print("Sztos")
-        # drone.takeoff()
-        # drone.wait(5)
-        drone.land()
+
 
 
 
 
 except KeyboardInterrupt:
-    drone.EmergencyCall()
+    # drone.EmergencyCall()
+    drone.streamoff()
 
 
-# drone.streamoff()
-
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     drone.streamoff()
 
 
