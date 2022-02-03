4f59e7c4e17436923f5925299ca6317a0fa96663
Arkadiusz Kruszynski <43806984+xxBeWolfxx@users.noreply.github.com>
2022-02-03 10:30:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from TelloBird import TelloBird
 import cv2
 import time
@@ -16,22 +12,15 @@ try:
     while True:
         if start == True:
             drone.streamon()
-            # drone.takeoff()
-            # print(drone.get_battery())
             start = False
-            #drone.squareMissionL(30)
-
 
         drone.watingForMission()
 
 
 
-
-
-
 except KeyboardInterrupt:
     drone.EmergencyCall()
-    drone.streamoff()
+
 
 
 
